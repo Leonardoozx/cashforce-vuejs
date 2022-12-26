f23d5a59d78337dc17f5ba2781753154375208c7
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-12-26 18:24:41 -0300

    feat: adds the `/orders` routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 
+const orderRoutes = require('./Routes/order.routes');
+
 const app = express();
 
 app.options('*', cors());
 app.use(cors());
 
 app.use(express.json());
+
+app.use('/orders', orderRoutes);
 
 module.exports = app;
