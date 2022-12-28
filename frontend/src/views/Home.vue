3c3125c5b8f9d9c0cdc0416997762ba234b0ffb1
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-12-28 18:40:43 -0300

    feat: the application home `view`

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <hr id="hr" />
+    <Header v-bind:title="title" v-bind:subtitle="subtitle" />
+  </main>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      title: "Bem vindo à Cashforce",
+      subtitle: "Você tem a forca. Multiplique-a."
+      };
+
+  },
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+main {
+  height: 100vh;
+  background-color: #ffffff;
+  width: 87.7%;
+  box-shadow: 0px 10px 30px rgba(225, 229, 236, 0.5);
+  float: right;
+}
+
+#hr {
+  margin-top: 50px;
+  padding: 0px;
+  border: 1px solid rgb(239, 240, 245);
+}
+</style>
