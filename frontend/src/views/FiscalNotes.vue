6daf5af7ae939733fd46fa1d388b5abfdb0eb4bb
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-12-28 18:40:29 -0300

    feat: a `view` that renders `fiscal-notes` path page

---
+++ frontend/src/views/FiscalNotes.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <hr id="hr" />
+    <Header v-bind:title="title" v-bind:subtitle="subtitle" />
+    <FiscalNotesCard />
+  </main>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import FiscalNotesCard from "../components/FiscalNotesCard.vue";
+
+export default {
+  name: "FiscalNotes",
+  data() {
+    return {
+      title: "Notas fiscais",
+      subtitle: "Visualize as notas fiscais que você tem.",
+    };
+  },
+  components: {
+    Header,
+    FiscalNotesCard,
+  },
+};
+</script>
+
+<style>
+main {
+  height: 100vh;
+  background-color: #ffffff;
+  width: 87.7%;
+  box-shadow: 0px 10px 30px rgba(225, 229, 236, 0.5);
+  float: right;
+}
+
+#hr {
+  margin-top: 50px;
+  padding: 0px;
+  border: 1px solid rgb(239, 240, 245);
+}
+</style>
