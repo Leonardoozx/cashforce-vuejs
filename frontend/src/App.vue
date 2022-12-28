e2bb033d53baf67cb6d84f74ce28e128fccb2090
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-12-28 18:39:04 -0300

    feat: adds routes to the application

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,23 @@
 <template>
   <div class="root-container">
     <SideBar />
-    <main>
-      <hr id="hr" />
-      <Header />
-      <FiscalNotesCard />
-    </main>
+    <router-view />
   </div>
 </template>
 
 <script>
-import SideBar from './components/SideBar.vue';
-import Header from './components/Header.vue';
-import FiscalNotesCard from './components/FiscalNotesCard.vue';
+import SideBar from "./components/SideBar.vue";
+import Header from "./components/Header.vue";
+import FiscalNotesCard from "./components/FiscalNotesCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     SideBar,
     Header,
-    FiscalNotesCard
-  }
-}
+    FiscalNotesCard,
+  },
+};
 </script>
 
 <style>
@@ -35,17 +31,7 @@ export default {
   background-color: grey;
 }
 
-main {
-  height: 100vh;
-  background-color: #ffffff;
-  width: 87.7%;
-  box-shadow: 0px 10px 30px rgba(225, 229, 236, 0.5);
-  float: right;
-}
-
-#hr {
-  margin-top: 50px;
-  padding: 0px;
-  border: 1px solid rgb(239, 240, 245);
+a {
+  text-decoration: none;
 }
 </style>
